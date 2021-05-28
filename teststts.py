870a9c3cf9ab9b508279a495300fc945070cd67a
patrick-setu <79290075+patrick-setu@users.noreply.github.com>
2021-05-28 13:09:52 +1200

    Add files via upload

---
+++ teststts.py
@@ -0,0 +1,13 @@
+print("Hello.")
+
+name = input("What is your name?")
+thing = input("What are you doing right now?")
+print("Well {},  doing {} is pretty cool!" .format(name, thing))
+
+weather = input("What is the weather like?")
+
+if weather == "sunny":
+    print("That's pretty cool I guess.")
+
+else:
+    print("Uhhhh, okay.")
