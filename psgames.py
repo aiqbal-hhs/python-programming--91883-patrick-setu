1975ca3dea025b55d9a2556014e67009bac71ba5
patrick-setu <79290075+patrick-setu@users.noreply.github.com>
2021-06-29 09:48:54 +1200

    Update psgames.py

--- psgames.py
+++ psgames.py
@@ -6,4 +6,11 @@ want_ps4 = int(input("How many ps4 games would you like?"))
 
 total_cost = ((want_ps3 * ps3_game) + (want_ps4 * 45))
 
-print("The total cost for your order is: ${}." .format(total_cost))
+if total_cost >= 70:
+    print("You are now eligible for a 17% discount on your total cost.")
+    total_cost *= 0.83
+elif total_cost >= 100:
+    print("You are now eligble for a 20% discount on your total cost.")
+    total_cost *= 0.8
+    
+print("The total cost for your order is: ${}" .format(total_cost))
